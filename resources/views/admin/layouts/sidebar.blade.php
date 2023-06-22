f8de7b3101cd24980b955ff93129e9efaf9f55bf
Kris Kurniawan <kris7kurniawan@gmail.com>
2023-06-22 15:59:38 +0900

    Update Icon

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -10,9 +10,9 @@
             {{-- <li class="menu-header">Dashboard</li> --}}
             <li><a class="nav-link" href="{{ route('dashboard') }}"><i class="fas fa-fire"></i> <span>Dashboard</span></a></li>
             <li class="menu-header">Menu</li>
-            <li><a class="nav-link" href="{{ route('datatable.index') }}"><i class="far fa-square"></i> <span>Datatable</span></a></li>
-            <li><a class="nav-link" href="{{ route('chartjs.index') }}"><i class="far fa-square"></i> <span>ChartJs</span></a></li>
+            <li><a class="nav-link" href="{{ route('datatable.index') }}"><i class="fas fa-table"></i> <span>Datatable</span></a></li>
+            <li><a class="nav-link" href="{{ route('chartjs.index') }}"><i class="fas fa-desktop"></i> <span>ChartJs</span></a></li>
             <li class="menu-header">Akses</li>
-            <li><a class="nav-link" href="{{ route('pengguna.index') }}"><i class="far fa-square"></i> <span>Pengguna</span></a></li>
+            <li><a class="nav-link" href="{{ route('pengguna.index') }}"><i class="fas fa-user"></i> <span>Pengguna</span></a></li>
     </aside>
 </div>
